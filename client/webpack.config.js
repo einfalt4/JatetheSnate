040600426ff3614482af6dc2bde94acb88606aa1
einfalt4 <107439535+einfalt4@users.noreply.github.com>
2022-12-02 11:17:54 -0500

    JS, webpack etc

---
+++ client/webpack.config.js
@@ -0,0 +1,60 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const path = require('path');
+const { InjectManifest } = require('workbox-webpack-plugin');
+
+// TODO: Add and configure workbox plugins for a service worker and manifest file.
+// TODO: Add CSS loaders and babel to webpack.
+
+module.exports = () => {
+  return {
+    mode: 'development',
+    entry: {
+      main: './src/js/index.js',
+      install: './src/js/install.js'
+    },
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Just Another Text Editor'
+      }),
+
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Just Another Text Editor',
+        short_name: "Jate",
+        description: 'another text editor, like I keep saying',
+        background_color: '#145876',
+        theme_color: '#145876',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+
+
+
+      })
+    ],
+
+    module: {
+      rules: [
+        
+      ],
+    },
+  };
+};
