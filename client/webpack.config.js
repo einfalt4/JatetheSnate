d24ea288a5c5d7aa2a0a44da0a42c5d48894c2c8
einfalt4 <107439535+einfalt4@users.noreply.github.com>
2022-12-02 11:39:53 -0500

    webpack updated

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -53,7 +53,24 @@ module.exports = () => {
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: [
+                '@babel/pluin-proposal-object-rest-spread',
+                '@babel/transform-runtime',
+              ],
+            },
+          },
+        },
       ],
     },
   };
